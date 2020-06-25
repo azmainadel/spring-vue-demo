aaa2ee52fbdfcbecf95702d99d37fe581242af0a
Azmain Adel <azmainadel47@gmail.com>
2020-06-25 11:12:25 +0600

    :zap: Create demo Todo model class

---
+++ backend/src/main/java/com/azmainadel/todo/model/TodoModel.java
@@ -0,0 +1,54 @@
+package com.azmainadel.todo.model;
+
+//
+// Created by Azmain Adel on 6/25/20.
+// Copyright (c) 2020 Field Buzz. All rights reserved.
+//
+
+import org.springframework.data.annotation.Id;
+
+public class TodoModel {
+
+    @Id
+    private String id;
+    private String title;
+    private boolean completed;
+
+    public TodoModel(String title, boolean completed) {
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "TodoModel{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
+}
