61840adacf07306f81706c16feb9ce5d88bbd65e
Azmain Adel <azmainadel47@gmail.com>
2020-06-25 11:13:10 +0600

    :zap: Add inherited Exception class

---
+++ backend/src/main/java/com/azmainadel/todo/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.azmainadel.todo.exception;
+
+//
+// Created by Azmain Adel on 6/25/20.
+// Copyright (c) 2020 Field Buzz. All rights reserved.
+//
+
+public class EntityNotFoundException extends RuntimeException {
+}
