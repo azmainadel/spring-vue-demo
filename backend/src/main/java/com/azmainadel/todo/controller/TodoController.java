d5f5ee6c20d016fd06f8c12962fd76d3c2643630
Azmain Adel <azmainadel47@gmail.com>
2020-06-25 11:13:26 +0600

    :zap: Create controller class

---
+++ backend/src/main/java/com/azmainadel/todo/controller/TodoController.java
@@ -0,0 +1,45 @@
+package com.azmainadel.todo.controller;
+
+//
+// Created by Azmain Adel on 6/25/20.
+// Copyright (c) 2020 Field Buzz. All rights reserved.
+//
+
+import com.azmainadel.todo.model.TodoModel;
+import com.azmainadel.todo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todo")
+class TodoController {
+    @Autowired
+    private TodoService service;
+
+    @GetMapping
+    public List<TodoModel> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public TodoModel findById(@PathVariable String id) {
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public TodoModel create(@RequestBody TodoModel toDo) {
+        return service.save(toDo);
+    }
+
+    @PutMapping("/{id}")
+    public TodoModel update(@RequestBody TodoModel toDo) {
+        return service.save(toDo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable String id) {
+        service.deleteById(id);
+    }
+}
